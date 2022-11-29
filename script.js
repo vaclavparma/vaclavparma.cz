67733b4661f4f456868a79fa77ca987df667ed6c
VaclavParma <79199501+Venca321@users.noreply.github.com>
2022-11-29 17:37:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ setInterval(() => {
     var startDate = new Date("2006/02/08");
     var diffDate = new Date(new Date() - startDate);
     my_age = (diffDate.toISOString().slice(0, 4) - 1970) + diffDate.getMonth()/12 + (diffDate.getDate()-1)/365 + diffDate.getHours()/8760 + diffDate.getMinutes()/525600 + diffDate.getSeconds()/31536000 + diffDate.getMilliseconds()/31536000000
-    my_age = Math.round(my_age * 1000000000000) / 1000000000000
-    my_age = my_age.toFixed(12)
+    my_age = Math.round(my_age * 1000000000) / 1000000000
+    my_age = my_age.toFixed(9)
     var element = document.getElementById("age");
     element.textContent = my_age;
 }, 100)
