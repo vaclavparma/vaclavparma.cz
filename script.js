2672ac7b99ef73d135e39be4f69a09589f37cd28
VaclavParma <79199501+Venca321@users.noreply.github.com>
2022-11-30 15:48:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,30 +7,37 @@ setInterval(() => {
     my_age = my_age.toFixed(9)
     var element = document.getElementById("age");
     element.textContent = my_age;
-}, 100)
+}, 100);
 
 underlined_is_on = true
 underlined_old = "none"
 function underline_menu(you_are_in){
-    if(you_are_in == "start"){underlined = "main_page_id--off"} //vypnuto
-    else if(you_are_in == "about"){underlined = "about_id"} //, age()
+    if(you_are_in == "start"){underlined = "main_page_id--off"}  //vypnuto
+    else if(you_are_in == "about"){underlined = "about_id"}
     else if (you_are_in == "projects"){underlined = "projects_id"}
     else if (you_are_in == "kontakt"){underlined = "kontakt_id"}
+
+    if (underlined == "main_page_id--off"){
+        var element = document.getElementById("menu");
+        element.style.background = "#1a263000"}
+    else{
+        var element = document.getElementById("menu");
+        element.style.background = "#1a2630"}
 
     try{
         if (underlined_is_on){
         var element = document.getElementById(underlined_old);
-        element.style.textDecoration = "none";
-        element.style.color = "#BCC8C6"}}
+        element.style.color = "#BCC8C6";
+        element.style.borderBottom = "2px solid transparent";}}
     catch{}
     try{
         if (underlined_is_on){
         var element = document.getElementById(underlined);
-        element.style.textDecoration = "underline";
-        element.style.color = "#3185FC"}}
+        element.style.color = "#3185FC";
+        element.style.borderBottom = "2px solid #3185FC";}}
     catch{}
     underlined_old = underlined
-    }
+}
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -52,16 +59,16 @@ function menu_off(){
     underlined_is_on = false
     try{
     var element = document.getElementById(underlined);
-    element.style.textDecoration = "none";
-    element.style.color = "#BCC8C6"}
+    element.style.color = "#BCC8C6";
+    element.style.borderBottom = "2px solid transparent";}
     catch{}
 };
 function menu_on(){
     underlined_is_on = true
     try{
     var element = document.getElementById(underlined);
-    element.style.textDecoration = "underline";
-    element.style.color = "#3185FC"}
+    element.style.color = "#3185FC";
+    element.style.borderBottom = "2px solid #3185FC";}
     catch{}
 };
 
@@ -69,11 +76,9 @@ function menu_on(){
 const about_el = document.getElementById('about_id');
 about_el.addEventListener('mouseover', function handleMouseOver() {
     menu_off()
-    about_el.style.textDecoration = "underline";
     about_el.style.color = "#3185FC"
 });
 about_el.addEventListener('mouseout', function handleMouseOut() {
-    about_el.style.textDecoration = "none";
     about_el.style.color = "#BCC8C6"
     menu_on()
 });
@@ -82,11 +87,9 @@ about_el.addEventListener('mouseout', function handleMouseOut() {
 const projects_el = document.getElementById('projects_id');
 projects_el.addEventListener('mouseover', function handleMouseOver() {
     menu_off()
-    projects_el.style.textDecoration = "underline";
     projects_el.style.color = "#3185FC"
 });
 projects_el.addEventListener('mouseout', function handleMouseOut() {
-    projects_el.style.textDecoration = "none";
     projects_el.style.color = "#BCC8C6"
     menu_on()
 });
@@ -95,22 +98,9 @@ projects_el.addEventListener('mouseout', function handleMouseOut() {
 const kontakt_el = document.getElementById('kontakt_id');
 kontakt_el.addEventListener('mouseover', function handleMouseOver() {
     menu_off()
-    kontakt_el.style.textDecoration = "underline";
     kontakt_el.style.color = "#3185FC"
 });
 kontakt_el.addEventListener('mouseout', function handleMouseOut() {
-    kontakt_el.style.textDecoration = "none";
     kontakt_el.style.color = "#BCC8C6"
     menu_on()
-});
-
-//Jazyk
-const language_el = document.getElementById('language-button');
-language_el.addEventListener('mouseover', function handleMouseOver() {
-    language_el.style.textDecoration = "underline";
-    language_el.style.color = "#3185FC"
-});
-language_el.addEventListener('mouseout', function handleMouseOut() {
-    language_el.style.textDecoration = "none";
-    language_el.style.color = "#BCC8C6"
 });
