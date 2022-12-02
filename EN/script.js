409ba771a55ad7e8f2be7ecf3fdc185dde63a82e
VaclavParma <79199501+Venca321@users.noreply.github.com>
2022-12-02 12:45:18 +0100

    Add files via upload

--- EN/script.js
+++ EN/script.js
@@ -58,6 +58,17 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 
+function OnClick(e){
+    var d=document.createElement("div");
+    d.className="OnClick";
+    d.style.top=e.clientY+"px";
+    d.style.left=e.clientX+"px";
+    document.body.appendChild(d);
+    d.addEventListener('animationend',function(){d.parentElement.removeChild(d);}.bind(this));
+}
+
+document.addEventListener('click', OnClick);
+
 //Menu
 const el = document.getElementById('menu');
 function menu_off(){
