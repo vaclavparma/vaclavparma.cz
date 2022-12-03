ddb7827af2c33947a4c0302e727da45c072ce555
VaclavParma <79199501+Venca321@users.noreply.github.com>
2022-12-03 09:42:37 +0100

    Small age correction

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 setInterval(() => {
-    const date_fix = 0.00125570776255707762557077625571;
+    const date_fix = 0.00262377367100159707962582705909;
     var startDate = new Date("2006/02/08");
     var diffDate = new Date(new Date() - startDate);
-    my_age = (diffDate.toISOString().slice(0, 4) - 1970) + diffDate.getMonth()/12 + (diffDate.getDate()-1)/365 + diffDate.getHours()/8760 + diffDate.getMinutes()/525600 + diffDate.getSeconds()/31536000 + diffDate.getMilliseconds()/31536000000
+    my_age = (diffDate.toISOString().slice(0, 4) - 1970) + diffDate.getMonth()/12 + (diffDate.getDate()-1)/365.25 + diffDate.getHours()/8760 + diffDate.getMinutes()/525600 + diffDate.getSeconds()/31536000 + diffDate.getMilliseconds()/31536000000
     my_age = Math.round((my_age+date_fix) * 1000000000) / 1000000000
     my_age = my_age.toFixed(9)
     var element = document.getElementById("age");
