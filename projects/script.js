fd7c71da490e471e9e86304a7521fbfc82047e5f
Venca321 <login@vaclavparma.cz>
2023-08-29 12:38:15 +0200

    Fix bugs

--- projects/script.js
+++ projects/script.js
@@ -10,14 +10,3 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
-
-function OnClick(e){
-    var d=document.createElement("div");
-    d.className="OnClick";
-    d.style.top=e.clientY+"px";
-    d.style.left=e.clientX+"px";
-    document.body.appendChild(d);
-    d.addEventListener('animationend',function(){d.parentElement.removeChild(d);}.bind(this));
-}
-
-document.addEventListener('click', OnClick);
