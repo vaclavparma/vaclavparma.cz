41c0feea3dd42c9015145de0fc90641abf0a1c07
Venca321 <login@vaclavparma.cz>
2024-02-25 14:17:04 +0100

    Start new design

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 setInterval(() => {
     const date_fix = 0.002819741;
-    var startDate = new Date("2006/02/08");
-    var diffDate = new Date(new Date() - startDate);
+    const startDate = new Date("2006/02/08");
+    const diffDate = new Date(new Date() - startDate);
     my_age =
       diffDate.toISOString().slice(0, 4)
       - 1970 
@@ -13,23 +13,49 @@ setInterval(() => {
       + diffDate.getMilliseconds() / 31536000000;
     my_age = Math.round((my_age - date_fix) * 1000000000) / 1000000000;
     my_age = my_age.toFixed(9);
-    var element = document.getElementById("age");
+    const element = document.getElementById("age");
     element.textContent = my_age;
 }, 100);
 
 
-function navbar() {
-  const about = document.getElementById("about");
-  const windowHeight = window.innerHeight;
-  const elementTop = about.getBoundingClientRect().top;
 
-  const navbar = document.getElementById("navbar");
-  if (elementTop < windowHeight) {
-    navbar.style.background = "rgb(7, 15, 23)";
-  }
-  else {
-    navbar.style.background = "rgb(7, 15, 23, 0)";
+const speed = 100;
+let txt = '';
+let i = 0;
+let wordCounter = 1;
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("text-animation").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
   }
 }
 
-window.addEventListener("scroll", navbar);
+window.onload = (event) => {
+  const element = document.getElementById("text-animation");
+  const words = element.textContent.split(', ')
+  element.textContent = words[0];
+  
+  setTimeout(() => {
+    txt = "";
+    i = 0;
+    element.textContent = "";
+  }, 2250);
+
+  setInterval(() => {
+    const element = document.getElementById("text-animation");
+    if (words.length != 0) {
+      if (wordCounter == words.length) {
+        wordCounter = 0;
+      }
+      txt = words[wordCounter];
+      wordCounter++;
+      typeWriter();
+      setTimeout(() => {
+        txt = "";
+        i = 0;
+        element.textContent = "";
+      }, 2250);
+    } 
+  }, 2500);
+};
