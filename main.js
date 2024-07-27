6d5ab60ef616451bc65994a1cb9f49d70655b84a
Václav Parma <login@vaclavparma.cz>
2024-07-27 18:16:52 +0200

    Add stars parallax efect

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-// scripts/main.js
+// main.js
 
-/*window.onload = function () {
-  // Zavoláme funkce pro generování hvězd
+window.onload = function () {
   createStars();
-  createShootingStars();
-};*/
+  setTimeout(generateShootingStars, 2000); // Zpoždění 2 sekundy pro padající hvězdy
+};
+
+window.addEventListener("scroll", function () {
+  const scrollPosition = window.pageYOffset;
+  const smallStars = document.querySelectorAll(".small-star");
+  const mediumStars = document.querySelectorAll(".medium-star");
+  const largeStars = document.querySelectorAll(".large-star");
+
+  smallStars.forEach((star) => {
+    star.style.transform = `translateY(${-scrollPosition * 0.05}px)`;
+  });
+
+  mediumStars.forEach((star) => {
+    star.style.transform = `translateY(${-scrollPosition * 0.1}px)`;
+  });
+
+  largeStars.forEach((star) => {
+    star.style.transform = `translateY(${-scrollPosition * 0.15}px)`;
+  });
+});
