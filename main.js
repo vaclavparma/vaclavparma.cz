e8be51a0d1dc45b0976352b180e6ac3cdbd10fc4
Václav Parma <login@vaclavparma.cz>
2024-08-08 18:29:18 +0200

    Start about section

--- main.js
+++ main.js
@@ -10,4 +10,23 @@ document.addEventListener("DOMContentLoaded", function () {
       scrollArrow.style.opacity = 0;
     }
   });
+
+  setInterval(() => {
+    const date_fix = 0.002819741;
+    const startDate = new Date("2006/02/08");
+    const diffDate = new Date(new Date() - startDate);
+    my_age =
+      diffDate.toISOString().slice(0, 4) -
+      1970 +
+      diffDate.getMonth() / 12 +
+      (diffDate.getDate() - 1) / 365.25 +
+      diffDate.getHours() / 8760 +
+      diffDate.getMinutes() / 525600 +
+      diffDate.getSeconds() / 31536000 +
+      diffDate.getMilliseconds() / 31536000000;
+    my_age = Math.round((my_age - date_fix) * 1000000000) / 1000000000;
+    my_age = my_age.toFixed(9);
+    const element = document.getElementById("age");
+    element.textContent = my_age;
+  }, 100);
 });
