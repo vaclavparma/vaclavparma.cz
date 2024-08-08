e5013ba6649bbf0fbb78edc47c11009260c1d46b
Václav Parma <login@vaclavparma.cz>
2024-08-08 18:11:10 +0200

    Add nav, add scroll arrow

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // main.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    const scrollArrow = document.getElementById("scroll-arrow");
+    const scrollPosition = window.scrollY;
+    const windowHeight = window.innerHeight;
+
+    if (scrollPosition > windowHeight * 0.05) {
+      scrollArrow.style.opacity = 0;
+    }
+  });
+});
