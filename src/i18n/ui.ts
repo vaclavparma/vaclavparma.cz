e78e17edcb1f081fc59e2707c4aeee68290598bd
Václav Parma <login@vaclavparma.cz>
2024-08-18 15:39:24 +0200

    Add i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,45 @@
+export const languages = {
+  cs: "Čeština",
+  en: "English",
+};
+
+export const defaultLang = "cs";
+
+export const ui = {
+  cs: {
+    "nav.label.home": "Sekce domů",
+    "nav.label.about": "Sekce o mně",
+    "nav.label.experience": "Sekce zkušenosti",
+    "nav.label.email": "Pošlete mi email",
+    "nav.label.github": "Navštivte můj GitHub",
+    "nav.label.linkedin": "Navštivte můj LinkedIn",
+    "home.subtitle": "Student, programátor, maker",
+    "about.title": "O mně",
+    "about.1.0": "Jmenuji se Václav Parma, je mi ",
+    "about.1.1":
+      " let a jsem programátor, student a maker z České republiky. Studuji na Gymnáziu Václava Hlavatého v Lounech, kde se kromě studia věnuji svým zájmům – programování a práci na různých elektronických projektech.",
+    "about.2":
+      "Pracuji jako AI researcher a Python vývojář ve společnosti Oscillator Labs s.r.o., kde mám možnost nejen využívat své současné znalosti na řešení praktických problémů, ale také se neustále učit věcem.",
+    "about.3":
+      "Většinu svých znalostí jsem získal metodou pokus / omyl při práci na vlastních projektech. Jedním z nich je i tato webová stránka. Baví mě vytvářet menší programy, které mi pomáhají objevovat nové technologie a zároveň prohlubují mé stávající dovednosti. Občas se také věnuji hardwaru, například při experimentech s Arduinem nebo při stavbě 8-bitového počítače.",
+    "experience.title": "Zkušenosti",
+  },
+  en: {
+    "nav.label.home": "Home section of the page",
+    "nav.label.about": "About section of the page",
+    "nav.label.experience": "Work section of the page",
+    "nav.label.email": "Send me an email",
+    "nav.label.github": "Visit my GitHub",
+    "nav.label.linkedin": "Visit my LinkedIn",
+    "home.subtitle": "Student, programmer, maker",
+    "about.title": "About me",
+    "about.1.0": "My name is Vaclav Parma, I am ",
+    "about.1.1":
+      " years old and I am a programmer, student and maker from Czech Republic. I study at the Václav Hlavatý Gymnasium in Louny, where besides studying I am also focusing on my interests - programming and working on various electronic projects.",
+    "about.2":
+      "I work as an AI researcher and Python developer at Oscillator Labs s.r.o., where I have the opportunity not only to use my current knowledge to solve real-world problems, but also to constantly learn new things.",
+    "about.3":
+      "Most of my knowledge has been gained through trial / error while working on my own projects. One of them is this website. I enjoy creating smaller programs that help me discover new technologies while deepening my existing skills. I also occasionally work on hardware, such as experimenting with Arduino or building an 8-bit computer.",
+    "experience.title": "My experience",
+  },
+} as const;
