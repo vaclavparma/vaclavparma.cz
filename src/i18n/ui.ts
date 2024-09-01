fc8b492b23804e042451a752a081a1604b239f01
Václav Parma <login@vaclavparma.cz>
2024-09-01 20:08:21 +0200

    Add experience

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -26,6 +26,17 @@ export const ui = {
     "about.3":
       "Většinu svých znalostí jsem získal metodou pokus / omyl při práci na vlastních projektech. Jedním z nich je i tato webová stránka. Baví mě vytvářet menší programy, které mi pomáhají objevovat nové technologie a zároveň prohlubují mé stávající dovednosti. Občas se také věnuji hardwaru, například při experimentech s Arduinem nebo při stavbě 8-bitového počítače.",
     "experience.title": "Zkušenosti",
+    "experience.1.title": "Gymnázium Václava Hlavatého Louny",
+    "experience.1.description": "Nastoupil na osmileté studium na Gymnáziu Václava Hlavatého v Lounech.",
+    "experience.2.title": "První projekty v Pythonu",
+    "experience.2.description": "Začal první projekt v Pythonu a naučil jsem se tím programovat.",
+    "experience.3.title": "AI Researcher Oscillator Labs s.r.o.",
+    "experience.3.description": "Začal jsem uplatňovat a dále zdokonalovat své znalosti v Oscillator Labs s.r.o.",
+    "experience.4.title": "Ročníková Práce",
+    "experience.4.description":
+      'Napsal ročníkovou práci Detekce gest pomocí počítačového vidění ve hře "kámen, nůžky, papír".',
+    "experience.5.title": "Microsoft Certified: Azure Fundamentals",
+    "experience.5.description": "Získal jsem Azure Fundamentals certifikaci.",
   },
   en: {
     description:
@@ -47,5 +58,16 @@ export const ui = {
     "about.3":
       "Most of my knowledge has been gained through trial / error while working on my own projects. One of them is this website. I enjoy creating smaller programs that help me discover new technologies while deepening my existing skills. I also occasionally work on hardware, such as experimenting with Arduino or building an 8-bit computer.",
     "experience.title": "My experience",
+    "experience.1.title": "Gymnázium Václava Hlavatého Louny",
+    "experience.1.description": "I started my eight-year studies at the Gymnázium Václava Hlavatého Louny.",
+    "experience.2.title": "First projects in Python",
+    "experience.2.description": "I started my first project in Python and learned how to code with it.",
+    "experience.3.title": "AI Researcher Oscillator Labs s.r.o.",
+    "experience.3.description": "I started to apply and further improve my knowledge at Oscillator Labs s.r.o.",
+    "experience.4.title": "High school research paper",
+    "experience.4.description":
+      "I wrote a paper Gesture recognition via computer vision in the game Rock, paper, scissors.",
+    "experience.5.title": "Microsoft Certified: Azure Fundamentals",
+    "experience.5.description": "I got my Azure Fundamentals certification.",
   },
 } as const;
