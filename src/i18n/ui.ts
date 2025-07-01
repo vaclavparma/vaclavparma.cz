0e5b11d01ad06bb1a190da84b48d1af8262f1d9f
Václav Parma <github@vaclavparma.cz>
2025-07-01 15:03:11 +0200

    Update outdated texts

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -20,9 +20,9 @@ export const ui = {
     "about.title": "O mně",
     "about.1.0": "Jmenuji se Václav Parma, je mi ",
     "about.1.1":
-      " let a jsem programátor, student a maker z České republiky. Studuji na Gymnáziu Václava Hlavatého v Lounech, kde se kromě studia věnuji svým zájmům – programování a práci na různých elektronických projektech.",
+      " let a jsem student, programátor a maker z České republiky. Studuji na Fakultě informačních technologií ČVUT v Praze.",
     "about.2":
-      "Pracuji jako AI researcher a Python vývojář ve společnosti Oscillator Labs s.r.o., kde mám možnost nejen využívat své současné znalosti na řešení praktických problémů, ale také se neustále učit věcem.",
+      "Pracuji jako CTO (Chief Technology Officer) ve společnosti Oscillator Labs s.r.o., kde mám možnost nejen využívat své dosavadní znalosti k řešení praktických problémů, ale také se neustále učit novým věcem.",
     "about.3":
       "Většinu svých znalostí jsem získal metodou pokus / omyl při práci na vlastních projektech. Jedním z nich je i tato webová stránka. Baví mě vytvářet menší programy, které mi pomáhají objevovat nové technologie a zároveň prohlubují mé stávající dovednosti. Občas se také věnuji hardwaru, například při experimentech s Arduinem nebo při stavbě 8-bitového počítače.",
     "experience.title": "Zkušenosti",
@@ -30,13 +30,13 @@ export const ui = {
     "experience.1.description": "Nastoupil na osmileté studium na Gymnáziu Václava Hlavatého v Lounech.",
     "experience.2.title": "První projekty v Pythonu",
     "experience.2.description": "Začal první projekt v Pythonu a naučil jsem se tím programovat.",
-    "experience.3.title": "AI Researcher Oscillator Labs s.r.o.",
+    "experience.3.title": "Python developer & AI Researcher @ Oscillator Labs s.r.o.",
     "experience.3.description": "Začal jsem uplatňovat a dále zdokonalovat své znalosti v Oscillator Labs s.r.o.",
-    "experience.4.title": "Ročníková Práce",
-    "experience.4.description":
-      'Napsal ročníkovou práci Detekce gest pomocí počítačového vidění ve hře "kámen, nůžky, papír".',
-    "experience.5.title": "Microsoft Certified: Azure Fundamentals",
-    "experience.5.description": "Získal jsem Azure Fundamentals certifikaci.",
+    "experience.4.title": "Microsoft Certified: Azure Fundamentals",
+    "experience.4.description": "Získal jsem Azure Fundamentals certifikaci.",
+    "experience.5.title": "CTO @ Oscillator Labs s.r.o.",
+    "experience.5.description":
+      "Dále se zaměřuji na rozšiřování svých zkušeností a dovedností v oblasti vývoje softwaru.",
   },
   en: {
     description:
@@ -52,9 +52,9 @@ export const ui = {
     "about.title": "About me",
     "about.1.0": "My name is Vaclav Parma, I am ",
     "about.1.1":
-      " years old and I am a programmer, student and maker from Czech Republic. I study at the Václav Hlavatý Gymnasium in Louny, where besides studying I am also focusing on my interests - programming and working on various electronic projects.",
+      " years old and I am a student, programmer and maker from Czech Republic.  I study at the Faculty of Information Technology at the Czech Technical University in Prague.",
     "about.2":
-      "I work as an AI researcher and Python developer at Oscillator Labs s.r.o., where I have the opportunity not only to use my current knowledge to solve real-world problems, but also to constantly learn new things.",
+      "I work as CTO (Chief Technology Officer) at Oscillator Labs s.r.o., where I have the opportunity not only to use my existing knowledge to solve practical problems, but also to constantly learn new things.",
     "about.3":
       "Most of my knowledge has been gained through trial / error while working on my own projects. One of them is this website. I enjoy creating smaller programs that help me discover new technologies while deepening my existing skills. I also occasionally work on hardware, such as experimenting with Arduino or building an 8-bit computer.",
     "experience.title": "My experience",
@@ -62,12 +62,11 @@ export const ui = {
     "experience.1.description": "I started my eight-year studies at the Gymnázium Václava Hlavatého Louny.",
     "experience.2.title": "First projects in Python",
     "experience.2.description": "I started my first project in Python and learned how to code with it.",
-    "experience.3.title": "AI Researcher Oscillator Labs s.r.o.",
+    "experience.3.title": "Python developer & AI Researcher @ Oscillator Labs s.r.o.",
     "experience.3.description": "I started to apply and further improve my knowledge at Oscillator Labs s.r.o.",
-    "experience.4.title": "High school research paper",
-    "experience.4.description":
-      "I wrote a paper Gesture recognition via computer vision in the game Rock, paper, scissors.",
-    "experience.5.title": "Microsoft Certified: Azure Fundamentals",
-    "experience.5.description": "I got my Azure Fundamentals certification.",
+    "experience.4.title": "Microsoft Certified: Azure Fundamentals",
+    "experience.4.description": "I earned my Azure Fundamentals certification.",
+    "experience.5.title": "CTO @ Oscillator Labs s.r.o.",
+    "experience.5.description": "I continue to focus on expanding my experience and skills in software development.",
   },
 } as const;
